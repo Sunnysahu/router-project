2f2f44fd5b8d56e26b18e1d35bc39ac28942fd56
Sunny Sahu <sunnysahu8210@gmail.com>
2024-03-13 14:39:24 +0530

    Used Loader

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 function User() {
   const { id } = useParams();
-  return <div className="text-center">User : {id} </div>;
+  return <div className=" bg-gray-600 text-3xl text-center">User : {id} </div>;
 
   //   const params = useParams();
   //   return <div className="text-center">User : {params.id} </div>;
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import User from "./components/User/User.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -38,9 +39,12 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
-      <Route path="about" element={<About />} />
+      <Route path="about" element={<About />}>
+        <Route path="sunny" />
+      </Route>
       <Route path="contact" element={<Contact />} />
       <Route path="user/:id" element={<User />} />
+      <Route path="/github" element={<Github />} loader={githubInfoLoader} />
     </Route>
   )
 );
